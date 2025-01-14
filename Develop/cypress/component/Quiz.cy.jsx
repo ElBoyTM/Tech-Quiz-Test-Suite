82b0f00c562dfe695943797dc600457d354947c6
ElBoyTM <nszalenski@gmail.com>
2025-01-14 17:29:42 -0500

    added some prelim tests

--- Develop/cypress/component/Quiz.cy.jsx
+++ Develop/cypress/component/Quiz.cy.jsx
@@ -1,2 +1,15 @@
 import React from "react";
 import Quiz from "../../client/src/components/Quiz";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("Quiz Component", () => {
+    it("should render without throwing an error", () => {
+        cy.visit("http://localhost:3000/quiz");
+        cy.get("h1").should("have.text", "Quiz");
+    });
+
+    it("should render a button", () => {
+        cy.get("button").should("exist");
+    });
+});
